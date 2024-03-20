dc313a68e13f0ae6fd8700cd2eee22346d829e17
Tturna <42873350+Tturna@users.noreply.github.com>
2024-03-20 17:58:16 +0200

    form stuff and chart.js

---
+++ ASP-AJAX/Controllers/JimboController.cs
@@ -0,0 +1,25 @@
+﻿using ASP_AJAX.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP_AJAX.Controllers;
+
+public class JimboController : Controller
+{
+    [HttpGet]
+    public IActionResult Index()
+    {
+        return View();
+    }
+    
+    [HttpPost]
+    public IActionResult Index([FromForm] JimboModel data)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View(data);
+        }
+
+        JimboModel.AddJimbo(data);
+        return RedirectToAction("Index", "Home");
+    }
+}
---
+++ ASP-AJAX/Models/JimboModel.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ASP_AJAX.Models;
+
+public enum Hobby
+{
+    Reading,
+    Writing,
+    Programming,
+    Gaming,
+    Cooking,
+    Baking,
+    Drawing,
+    Painting,
+    Singing,
+    Dancing,
+    Sports,
+}
+
+public class JimboModel
+{
+    [Required]
+    [MinLength(3)]
+    [MaxLength(20)]
+    public string Name { get; set; }
+    [Required]
+    [Range(0, 120)]
+    public int Age { get; set; }
+    [Required]
+    [Range(0, 200)]
+    public int IQ { get; set; }
+    [Required]
+    public Hobby[] Hobbies { get; set; }
+    
+    [NotMapped]
+    public static readonly List<JimboModel> Jimbos = new();
+    
+    public static void AddJimbo(JimboModel jimbo)
+    {
+        Jimbos.Add(jimbo);
+    }
+}
